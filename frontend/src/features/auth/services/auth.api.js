86c33c440591670ea88e766b8fc9d2c8e276f5e1
Your Name <deepesh3950lodhi@gmail.com>
2026-09-14 04:07:33 +0530

    public folder update

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://resumebuilderproject-w9zi.onrender.com",
     withCredentials: true
 })
 
